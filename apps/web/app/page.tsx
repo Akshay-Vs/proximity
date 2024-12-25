52bb7f4c2719be0abcf365a87e2e692c1845adaf
Akshay Vs <me.akshay.vs@gmail.com>
2024-12-25 20:06:13 +0530

    feat: add shadcn input component

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,17 +1,22 @@
 import Header from '@/components/specific/header/nav-header';
 import Featured from '@/components/specific/news/featured/featured';
+import Search from '@/components/specific/search/search';
 
 import testArticle from '@/public/images/image 2.png';
 
 const Home = () => {
 	return (
-		<main className="w-full h-full flex flex-col gap-4 justify-center items-center">
-			<Header />
-			<Featured
-				title="Musk's $56bn deal rejected"
-				description="Tesla chief executive Elon Musk's record-breaking $56bn (£47bn) pay award will not be reinstated, a judge has ruled"
-				image={testArticle}
-			/>
+		<main className="w-full h-full flex flex-col gap-8 justify-center items-center">
+			<div className="w-full h-full flex flex-col gap-6 justify-center items-center">
+				<Header />
+				<Featured
+					title="Musk's $56bn deal rejected"
+					description="Tesla chief executive Elon Musk's record-breaking $56bn (£47bn) pay award will not be reinstated, a judge has ruled"
+					image={testArticle}
+				/>
+			</div>
+
+			<Search />
 		</main>
 	);
 };
