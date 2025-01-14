8b647152dcbb6695e42ebb63830c5cf84625ad98
Akshay Vs <me.akshay.vs@gmail.com>
2025-01-14 23:55:50 +0530

    feat: add compression, rare-limiting, and input validation

--- services/scraper/src/server.ts
+++ services/scraper/src/server.ts
@@ -1,20 +1,43 @@
 import Fastify from 'fastify'
+import fastifyCompress from '@fastify/compress'
+import fastifyRateLimit from '@fastify/rate-limit'
+import z from 'zod'
+
 import { Scraper } from './scraper'
 
 export const fastify = Fastify({
   logger: true
 })
 
+fastify.register(fastifyRateLimit, {
+  max: 50,
+  timeWindow: '1 minute'
+})
+
+
+fastify.register(
+  fastifyCompress,
+  { global: false }
+)
 fastify.get('/', async (_request, _reply) => {
   return { "service": "scraper", "status": "running", "version": "0.0.1", "message": "I scrape the news" }
 })
 
 fastify.post('/scrape', async (request, reply) => {
+  const bodySchema = z.object({
+    url: z.string().url(),
+  });
+
   try {
-    const { url } = request.body as { url: string };
-    const scraper = new Scraper(url);
+    const validatedBody = bodySchema.safeParse(request.body);
+
+    if (!validatedBody || !validatedBody.success) {
+      return await reply.status(400).send({ error: 'Invalid request body' });
+    }
+
+    const scraper = new Scraper(validatedBody.data.url);
     const result = await scraper.scrape();
-    return await result;
+    return result;
   }
   catch (error) {
     return await reply.status(500).send({ error: error });
