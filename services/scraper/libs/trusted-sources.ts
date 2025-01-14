064a2a490203a77f2cc8329aa20e161eb33ca1ab
Akshay Vs <me.akshay.vs@gmail.com>
2025-01-15 00:42:11 +0530

    feat: add trusted sources

---
+++ services/scraper/libs/trusted-sources.ts
@@ -0,0 +1,13 @@
+export const TRUSTED_SOURCES = [
+  'bbc.com',
+  'nytimes.com',
+  'cnn.com',
+  'reuters.com',
+  'theguardian.com',
+  'theverge.com',
+  'theatlantic.com',
+  'washingtonpost.com',
+  'wsj.com',
+  'bloomberg.com',
+  'cnbc.com',
+]
