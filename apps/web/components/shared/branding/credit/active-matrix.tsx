be1c2db0bbbd04dbc8252cf986f6db1ca113fb7e
Akshay Vs <me.akshay.vs@gmail.com>
2025-01-31 17:55:52 +0530

    feat: add credit tagline

---
+++ apps/web/components/shared/branding/credit/active-matrix.tsx
@@ -0,0 +1,9 @@
+const ActiveMatrix = () => {
+	return (
+		<p className="absolute bottom-4 left-1/2 -translate-x-1/2 select-none font-medium text-sm">
+			From Active Matrix
+		</p>
+	);
+};
+
+export default ActiveMatrix;
