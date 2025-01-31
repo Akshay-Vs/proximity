89080a1532800e80c4e079185709a62bf1f955a6
Akshay Vs <me.akshay.vs@gmail.com>
2025-01-31 20:17:58 +0530

    feat(web): add data provider utils

---
+++ apps/web/data/get-recomended-providers.tsx
@@ -0,0 +1,46 @@
+const getRecomendedProviders = async () => {
+	return [
+		{
+			id: 'tCV5HvjhrFj7UtCdECs74lthix7YbsZFHMvC53fkpBW1r2IO',
+			url: 'https://ko77xaoqa4.ufs.sh/f/tCV5HvjhrFj7UtCdECs74lthix7YbsZFHMvC53fkpBW1r2IO',
+		},
+		{
+			id: 'tCV5HvjhrFj779JC3c16RzKMGWPaoxlbmCA4V0N8Z3wUHTed',
+			url: 'https://ko77xaoqa4.ufs.sh/f/tCV5HvjhrFj779JC3c16RzKMGWPaoxlbmCA4V0N8Z3wUHTed',
+		},
+		{
+			id: 'tCV5HvjhrFj7ckBobqnNkxa7TBAh20FSOVg4Lldpt3eMjDKb',
+			url: 'https://ko77xaoqa4.ufs.sh/f/tCV5HvjhrFj7ckBobqnNkxa7TBAh20FSOVg4Lldpt3eMjDKb',
+		},
+		{
+			id: 'tCV5HvjhrFj7KMYiLGVPmdVsui3vJ1g7MYaA2LcfGqE5nlUo',
+			url: 'https://ko77xaoqa4.ufs.sh/f/tCV5HvjhrFj7KMYiLGVPmdVsui3vJ1g7MYaA2LcfGqE5nlUo',
+		},
+		{
+			id: 'tCV5HvjhrFj77j4bVgY16RzKMGWPaoxlbmCA4V0N8Z3wUHTe',
+			url: 'https://ko77xaoqa4.ufs.sh/f/tCV5HvjhrFj77j4bVgY16RzKMGWPaoxlbmCA4V0N8Z3wUHTe',
+		},
+		{
+			id: 'tCV5HvjhrFj7wYuhu4pRbcKf5Bzj6EXs14xoHqm3JwTk8aiV',
+			url: 'https://ko77xaoqa4.ufs.sh/f/tCV5HvjhrFj7wYuhu4pRbcKf5Bzj6EXs14xoHqm3JwTk8aiV',
+		},
+		{
+			id: 'tCV5HvjhrFj7OuuQmIs8U2XFd7vn9YBVWsxoaP4j3iD1byMr',
+			url: 'https://ko77xaoqa4.ufs.sh/f/tCV5HvjhrFj7OuuQmIs8U2XFd7vn9YBVWsxoaP4j3iD1byMr',
+		},
+		{
+			id: 'tCV5HvjhrFj70QUibYDrVW7IbkMm6Gnhdewja5PRCzAL3fDg',
+			url: 'https://ko77xaoqa4.ufs.sh/f/tCV5HvjhrFj70QUibYDrVW7IbkMm6Gnhdewja5PRCzAL3fDg',
+		},
+		{
+			id: 'tCV5HvjhrFj7Al0k2KoQ2M0UGsxT3Li1eYlgDhHwnmPkprWI',
+			url: 'https://ko77xaoqa4.ufs.sh/f/tCV5HvjhrFj7Al0k2KoQ2M0UGsxT3Li1eYlgDhHwnmPkprWI',
+		},
+		{
+			id: 'tCV5HvjhrFj7zmZOBBqwEZrYT1mnJ6XDvHc3A9yKM4tegq0S',
+			url: 'https://ko77xaoqa4.ufs.sh/f/tCV5HvjhrFj7zmZOBBqwEZrYT1mnJ6XDvHc3A9yKM4tegq0S',
+		},
+	];
+};
+
+export default getRecomendedProviders;
---
+++ apps/web/data/get-recomended-topics.tsx
@@ -0,0 +1,19 @@
+const getRecomendedTopics = async () => {
+	return [
+		{
+			id: 1,
+			label: 'Mars colonisation',
+		},
+		{
+			id: 2,
+			label: 'CRISPR Technology',
+		},
+		{
+			id: 3,
+			label: 'Quantum Computing',
+		},
+		{ id: 4, label: 'Nuclear Fusion' },
+	];
+};
+
+export default getRecomendedTopics;
