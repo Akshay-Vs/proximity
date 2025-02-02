d6fa3144aed2e561550402af983441678a561527
Akshay Vs <me.akshay.vs@gmail.com>
2025-02-02 13:30:21 +0530

    feat: add login page

---
+++ apps/web/app/(protected)/layout.tsx
@@ -0,0 +1,9 @@
+'use client';
+import { PropsWithChildren } from 'react';
+
+const Layout = ({ children }: PropsWithChildren) => {
+	//TODO : Add auth & access control
+	return <>{children}</>;
+};
+
+export default Layout;
