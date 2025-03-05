e0fb4ea87426802ab0493f55a76f44f4d2aecac5
Akshay-Vs <me.akshay.vs@gmail.com>
2025-03-05 21:25:11 +0530

    docs: add v2.0 changelogs

--- services/crawler/src/index.ts
+++ services/crawler/src/index.ts
@@ -4,32 +4,34 @@ import mq from '@proximity/rabbitmq-config'
 
 const urls = [
   'https://www.bbc.co.uk',
-  'https://spacenews.com',
-  'https://www.space.com',
-  'https://edition.cnn.com',
-  'https://www.timesnownews.com',
-  'https://www.nytimes.com',
-  'https://www.hindustantimes.com',
-  'https://www.theguardian.com',
-  'https://www.thehindu.com'
+  // 'https://spacenews.com',
+  // 'https://www.space.com',
+  // 'https://edition.cnn.com',
+  // 'https://www.timesnownews.com',
+  // 'https://www.nytimes.com',
+  // 'https://www.hindustantimes.com',
+  // 'https://www.theguardian.com',
+  // 'https://www.thehindu.com'
 ]
 const main = async () => {
   try {
     const results = await Promise.all(urls.map(async (url) => {
       const driver = new CheerioCrawler();
-      console.log("Starting crawler", url);
-      const links = await driver.crawl(url, 'article', '');
+      // console.log("Starting crawler", url);
+      const links = await driver.crawl(url, 'article', 'programmes');
       console.log("Successfully crawled", links.length, 'links from', url);
       return links;
     }));
 
     const flatedResults = results.flat(Infinity)
+    const providers = flatedResults.map(url => ({ driver: 'cheerio', url }))
+    console.log(providers)
 
     const connection = await amqp.connect(mq.url)
     const channel = await connection.createChannel()
     await channel.assertQueue(mq.queues.CrawledURLQueue)
 
-    channel.sendToQueue(mq.queues.CrawledURLQueue, Buffer.from(flatedResults.toString()))
+    channel.sendToQueue(mq.queues.CrawledURLQueue, Buffer.from(JSON.stringify({ providers })))
   }
   catch (err) {
     console.log(err)
