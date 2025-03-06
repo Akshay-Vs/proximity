8551c6445cda45aa90bf3e31cc9166e288bcf5ae
Akshay-Vs <me.akshay.vs@gmail.com>
2025-03-06 20:49:27 +0530

    feat: add mq config

---
+++ services/tldr/libs/mq.py
@@ -0,0 +1,7 @@
+host = "localhost"
+exchangeName = "proximity-exchange"
+CrawledURLQueue = "crawler-queue"
+ScrapedNewsQueue = "scraper-queue"
+SummarizerPriorityQueue = "summarizer-priority-queue"
+SummarizerNonPriorityQueue = "summarizer-non-priority-queue"
+SummarizedNewsQueue = "summarizer-completed-queue"
