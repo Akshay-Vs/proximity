168ef7f4ad1065f1a7e3002f077906f781615f3b
Akshay Vs <me.akshay.vs@gmail.com>
2025-03-10 19:01:08 +0530

    feat: add dontenv variables

--- services/tldr/libs/mq.py
+++ services/tldr/libs/mq.py
@@ -1,4 +1,12 @@
-host = "localhost"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+host = os.getenv("RABBITMQ_HOST")
+rabbitmq_user = os.getenv("RABBITMQ_DEFAULT_USER")
+rabbitmq_pass = os.getenv("RABBITMQ_DEFAULT_PASS")
+
 exchangeName = "proximity-exchange"
 CrawledURLQueue = "crawler-queue"
 ScrapedNewsQueue = "scraper-queue"
