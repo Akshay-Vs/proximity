eb824c783333b70bd28be664dbc3968f2d6b397c
Akshay-Vs <me.akshay.vs@gmail.com>
2025-03-16 23:37:37 +0530

    feat: integrate display topic store into HomeNewsGrid for dynamic news loading

--- apps/web/components/specific/home/home-news-grid/home-news-grid.tsx
+++ apps/web/components/specific/home/home-news-grid/home-news-grid.tsx
@@ -3,24 +3,30 @@ import { useEffect, useState } from 'react';
 
 import NewsCard, { NewsCardProps } from '../../../shared/news-card/news-card';
 import NewsCardSkeleton from '../../../shared/news-card/news-card-skeleton';
+import { useDisplayTopicStore } from '@/stores/display-topic-store';
 
 const HomeNewsGrid = () => {
 	const [news, setNews] = useState<NewsCardProps[] | 'loading'>([]);
+	const {topic, setLoading} = useDisplayTopicStore();
 
 	useEffect(() => {
 		setNews('loading');
-		// simulate loading
+		setLoading(topic)
+
+		// simulate loading delay
+		const timeoutDelay = topic === 'for-you' ? 4000 : 500;
 		setTimeout(() => {
+			
 			setNews([
-				// {
-				// 	image:
-				// 		'https://utfs.io/f/tCV5HvjhrFj7e9NgBZxqDOE9pI4s2uQrfZnahoL6R8vM03SV',
-				// 	title: 'Lockheed Martin challenges narrative on GPS vulnerability',
-				// 	publishedAt: '12 hours ago',
-				// 	source: 'CNN',
-				// 	views: '43k',
-				// 	slug: 'lockheed-martin-challenges-narrative-on-gps-vulnerability-124H8',
-				// },
+				{
+					image:
+						'https://utfs.io/f/tCV5HvjhrFj7e9NgBZxqDOE9pI4s2uQrfZnahoL6R8vM03SV',
+					title: 'Lockheed Martin challenges narrative on GPS vulnerability',
+					publishedAt: '12 hours ago',
+					source: 'CNN',
+					views: '43k',
+					slug: 'lockheed-martin-challenges-narrative-on-gps-vulnerability-124H8',
+				},
 				{
 					image:
 						'https://utfs.io/f/tCV5HvjhrFj78ppsNbrmDKZ6LVIqCe2WQuHiMNX41BYwsvEd',
@@ -31,8 +37,9 @@ const HomeNewsGrid = () => {
 					slug: 'india-eyes-record-year-for-space-with-10-planned-launches-GJ878G',
 				},
 			]);
-		}, 1000);
-	}, []);
+			setLoading(false)
+		}, timeoutDelay);
+	}, [setLoading, topic]);
 
 	return (
 		<section className="gap-6 px-[5%] pb-6 grid grid-cols-1 w-full h-full">
