606f7804b74e217ecc9155d9bf690818d6e80d54
Akshay-Vs <me.akshay.vs@gmail.com>
2025-03-17 19:40:01 +0530

    fix: enhance topic button hover state

--- apps/web/components/specific/home/topics/topic-button-primary.tsx
+++ apps/web/components/specific/home/topics/topic-button-primary.tsx
@@ -6,7 +6,7 @@ import type { ButtonBase } from '@/types/button';
 
 const TopicButtonPrimary = ({ label, isSelected,  isLoading, onClick }: ButtonBase) => {
 	return (
-		<Button variant="tag" onClick={onClick}className={cn("h-11 px-7", isLoading? "animate-pulse bg-primary opacity-80 text-white" :isSelected && "bg-primary text-white")}>
+		<Button variant="tag" onClick={onClick}className={cn("h-11 px-7", isLoading? "animate-pulse bg-primary opacity-80 hover:cursor-pointer text-white" :isSelected && "bg-primary text-white")}>
 			{label}
 		</Button>
 	);
