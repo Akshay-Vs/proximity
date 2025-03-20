c5c79e0492b337c090b96bbac0b88d7d541138c6
Akshay-Vs <me.akshay.vs@gmail.com>
2025-03-20 12:57:01 +0530

    feat: add news preview components with image, article, header, and footer

--- apps/web/components/specific/home/home-news-grid/home-news-grid.tsx
+++ apps/web/components/specific/home/home-news-grid/home-news-grid.tsx
@@ -23,17 +23,25 @@ const HomeNewsGrid = () => {
 					title:
 						'SpaceX letter criticizes FAA for “systemic challenges” in launch licensing',
 					publishedAt: '12 hours ago',
-					views: '43k',
+					reads: '43k',
 					slug: 'spacex-letter-criticizes-faa-for-systemic-challenges-in-launch-licensing-2vg45',
-					source: 'CNN',
+					source: {
+						name: 'CNN',
+						image: '',
+						url: 'https://www.cnn.com/',
+					},
 				},
 				{
 					image:
 						'https://utfs.io/f/tCV5HvjhrFj78ppsNbrmDKZ6LVIqCe2WQuHiMNX41BYwsvEd',
 					title: 'India eyes record year for space with 10 planned launches',
 					publishedAt: '5 hours ago',
-					source: 'Space News',
-					views: '28k',
+					source: {
+						name: 'CNN',
+						image: '',
+						url: 'https://www.cnn.com/',
+					},
+					reads: '28k',
 					slug: 'india-eyes-record-year-for-space-with-10-planned-launches-245626',
 				},
 				{
@@ -41,65 +49,13 @@ const HomeNewsGrid = () => {
 						'https://utfs.io/f/tCV5HvjhrFj7e9NgBZxqDOE9pI4s2uQrfZnahoL6R8vM03SV',
 					title: 'Lockheed Martin challenges narrative on GPS vulnerability',
 					publishedAt: '12 hours ago',
-					source: 'CNN',
-					views: '43k',
+					source: {
+						name: 'CNN',
+						image: '',
+						url: 'https://www.cnn.com/',
+					},
+					reads: '43k',
 					slug: 'lockheed-martin-challenges-narrative-on-gps-vulnerability-2456256',
-				},
-				{
-					image:
-						'https://ko77xaoqa4.ufs.sh/f/tCV5HvjhrFj7IdoJ5zuQoRFHJr4sv5UPGOuXKthkE83bW0i1',
-					title:
-						'SpaceX letter criticizes FAA for “systemic challenges” in launch licensing',
-					publishedAt: '12 hours ago',
-					views: '43k',
-					slug: 'spacex-letter-criticizes-faa-for-systemic-challenges-in-launch-licensing-124H8',
-					source: 'CNN',
-				},
-				{
-					image:
-						'https://utfs.io/f/tCV5HvjhrFj78ppsNbrmDKZ6LVIqCe2WQuHiMNX41BYwsvEd',
-					title: 'India eyes record year for space with 10 planned launches',
-					publishedAt: '5 hours ago',
-					source: 'Space News',
-					views: '28k',
-					slug: 'india-eyes-record-year-for-space-with-10-planned-launches-463653',
-				},
-				{
-					image:
-						'https://utfs.io/f/tCV5HvjhrFj7e9NgBZxqDOE9pI4s2uQrfZnahoL6R8vM03SV',
-					title: 'Lockheed Martin challenges narrative on GPS vulnerability',
-					publishedAt: '12 hours ago',
-					source: 'CNN',
-					views: '43k',
-					slug: 'lockheed-martin-challenges-narrative-on-gps-vulnerability-2342',
-				},
-				{
-					image:
-						'https://ko77xaoqa4.ufs.sh/f/tCV5HvjhrFj7IdoJ5zuQoRFHJr4sv5UPGOuXKthkE83bW0i1',
-					title:
-						'SpaceX letter criticizes FAA for “systemic challenges” in launch licensing',
-					publishedAt: '12 hours ago',
-					views: '43k',
-					slug: 'spacex-letter-criticizes-faa-for-systemic-challenges-in-launch-licensing-124H8',
-					source: 'CNN',
-				},
-				{
-					image:
-						'https://utfs.io/f/tCV5HvjhrFj78ppsNbrmDKZ6LVIqCe2WQuHiMNX41BYwsvEd',
-					title: 'India eyes record year for space with 10 planned launches',
-					publishedAt: '5 hours ago',
-					source: 'Space News',
-					views: '28k',
-					slug: 'india-eyes-record-year-for-space-with-10-planned-launches-2342',
-				},
-				{
-					image:
-						'https://utfs.io/f/tCV5HvjhrFj7e9NgBZxqDOE9pI4s2uQrfZnahoL6R8vM03SV',
-					title: 'Lockheed Martin challenges narrative on GPS vulnerability',
-					publishedAt: '12 hours ago',
-					source: 'CNN',
-					views: '43k',
-					slug: 'lockheed-martin-challenges-narrative-on-gps-vulnerability-45',
 				},
 			]);
 			setLoading(false);
@@ -118,7 +74,7 @@ const HomeNewsGrid = () => {
 							image={news.image}
 							publishedAt={news.publishedAt}
 							source={news.source}
-							views={news.views}
+							reads={news.reads}
 						/>
 					))}
 		</section>
--- apps/web/components/specific/news-grid/search-results-grid.tsx
+++ apps/web/components/specific/news-grid/search-results-grid.tsx
@@ -19,19 +19,38 @@ const SearchResultsGrid = () => {
 					title:
 						'SpaceX letter criticizes FAA for “systemic challenges” in launch licensing',
 					publishedAt: '12 hours ago',
-					views: '43k',
-					slug: 'spacex-letter-criticizes-faa-for-systemic-challenges-in-launch-licensing-124H8',
-					source: 'CNN',
+					reads: '43k',
+					slug: 'spacex-letter-criticizes-faa-for-systemic-challenges-in-launch-licensing-2vg45',
+					source: {
+						name: 'CNN',
+						image: '',
+						url: 'https://www.cnn.com/',
+					},
 				},
 				{
 					image:
 						'https://utfs.io/f/tCV5HvjhrFj78ppsNbrmDKZ6LVIqCe2WQuHiMNX41BYwsvEd',
 					title: 'India eyes record year for space with 10 planned launches',
 					publishedAt: '5 hours ago',
-					source: 'Space News',
-					views: '28k',
-					slug: 'india-eyes-record-year-for-space-with-10-planned-launches-GJ878G',
+					source: {
+						name: 'CNN',
+						image: '',
+						url: 'https://www.cnn.com/',
+					},					reads: '28k',
+					slug: 'india-eyes-record-year-for-space-with-10-planned-launches-245626',
 				},
+				{
+					image:
+						'https://utfs.io/f/tCV5HvjhrFj7e9NgBZxqDOE9pI4s2uQrfZnahoL6R8vM03SV',
+					title: 'Lockheed Martin challenges narrative on GPS vulnerability',
+					publishedAt: '12 hours ago',
+					source: {
+						name: 'CNN',
+						image: '',
+						url: 'https://www.cnn.com/',
+					},					reads: '43k',
+					slug: 'lockheed-martin-challenges-narrative-on-gps-vulnerability-2456256',
+				}
 			]);
 		}, 1000);
 	}, []);
@@ -48,7 +67,7 @@ const SearchResultsGrid = () => {
 							image={news.image}
 							publishedAt={news.publishedAt}
 							source={news.source}
-							views={news.views}
+							reads={news.reads}
 						/>
 					))}
 		</section>
