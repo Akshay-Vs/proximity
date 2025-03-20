fc287a96ca881de99bc318fb9359990582d14f08
Akshay-Vs <me.akshay.vs@gmail.com>
2025-03-20 15:35:23 +0530

    feat: update StyledInput component to support larger text size on large screens

--- apps/web/components/shared/input/styled-input.tsx
+++ apps/web/components/shared/input/styled-input.tsx
@@ -9,7 +9,7 @@ const StyledInput = ({
 		<div className="flex gap-2 center w-full h-14 rounded-[30px] border-2 border-border text-text px-4 mt-2">
 			<Input
 				{...props}
-				className="center leading-none h-9 w-full rounded-none border-none bg-transparent shadow-none md:text-3xl focus:outline-none focus-visible:ring-0 placeholder:text-[#626262] placeholder:font-medium px-0 py-0 disabled:opacity-100 cursor-pointer disabled:cursor-pointer"
+				className="center leading-none h-9 w-full rounded-none border-none bg-transparent shadow-none md:text-3xl lg:text-xl focus:outline-none focus-visible:ring-0 placeholder:text-[#626262] placeholder:font-medium px-0 py-0 disabled:opacity-100 cursor-pointer disabled:cursor-pointer"
 			/>
 			{children}
 		</div>
