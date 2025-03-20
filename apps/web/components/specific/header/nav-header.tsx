9590fc33b29694fc0818186478c5b2b37b62e3f4
Akshay-Vs <me.akshay.vs@gmail.com>
2025-03-20 15:38:20 +0530

    feat: integrate ExploreRedirectDesktop component into NavHeader for enhanced navigation

--- apps/web/components/specific/header/nav-header.tsx
+++ apps/web/components/specific/header/nav-header.tsx
@@ -2,12 +2,14 @@ import { Separator } from '@proximity/ui/shadcn/separator';
 
 import NavLogo from './nav-logo';
 import NavHamMenu from './nav-ham-menu';
+import ExploreRedirectDesktop from '../home/explore-redirect/explore-redirect-desktop';
 
 const Header = () => {
 	return (
 		<nav className="w-full flex flex-col h-fit col px-4 pt-4 gap-4">
 			<div className="flex flex-row justify-between items-center">
 				<NavLogo />
+				<ExploreRedirectDesktop />
 				<NavHamMenu />
 			</div>
 			<Separator />
