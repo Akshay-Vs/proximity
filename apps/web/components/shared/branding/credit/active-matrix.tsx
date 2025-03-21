af85727eb0ee248c0b459397236a9593a26fbe6b
Akshay-Vs <me.akshay.vs@gmail.com>
2025-03-21 12:54:00 +0530

    style: update ActiveMatrix component for full-width display and GPU transformation

--- apps/web/components/shared/branding/credit/active-matrix.tsx
+++ apps/web/components/shared/branding/credit/active-matrix.tsx
@@ -1,6 +1,6 @@
 const ActiveMatrix = () => {
 	return (
-		<p className="absolute bottom-4 select-none font-medium text-sm text-center w-screen xl:w-[40dvw]">
+		<p className="absolute bottom-4 select-none font-medium text-sm text-center w-full xl:w-[40dvw] transform-gpu">
 			From Active Matrix
 		</p>
 	);
